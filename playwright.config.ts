a0eb7c0f69dd3689e3fbca052ffcc9d025ee79e4
Kim Bell <krbell4@gmail.com>
2026-01-05 13:46:39 -0500

    update workers and delete example file

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   // workers: process.env.CI ? 1 : undefined, // change to 1 when network issues
-  workers: 1,
+  workers: 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
