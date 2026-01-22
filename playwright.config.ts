698a04180c4fa4263f186861267e102a9c5e5fc5
Kim Bell <krbell4@gmail.com>
2026-01-22 13:48:37 -0500

    configure workflow for snapshots

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,6 +24,8 @@ export default defineConfig({
   workers: process.env['CI'] ? 1 : 3, // change to 1 when network issues
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+  snapshotPathTemplate: '{testDir}/snapshot/{testFilePath}/{arg}{ext}',
+  outputDir: './test-results',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
